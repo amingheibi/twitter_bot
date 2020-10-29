7f9be6bb46183515406c62c1b43cc37ab9398517
amingheibi <amin.gheibi@gmail.com>
2020-10-28 22:07:30 -0400

    Wrtie tweets to a CSV file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import setting
 import requests
 import json
+import csv
 
 #sample query with curl, Twitter API v2
 # curl --location --request GET 'https://api.twitter.com/2/tweets/search/recent?query=nyc&tweet.fields=author_id,created_at,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source'
@@ -12,7 +13,8 @@ def endpoint_url():
 def get_params(query):
     params = (
      ('query', query),
-     ('tweet.fields', 'author_id,created_at,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source'),
+    #  ('tweet.fields', 'author_id,created_at,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source')
+     ('tweet.fields', 'author_id,entities')
     )
     return params
 
@@ -21,10 +23,18 @@ def get_headers():
     headers = {'authorization': 'Bearer {}'.format(setting.BEARER_TOKEN)}
     return headers
 
-response = requests.get(endpoint_url(),params=get_params('هوش مصنوعی'),headers=get_headers())
-if response.status_code !=200:
-    print(response.status_code)
-    print(response.text)
-else:
-    print(json.dumps(response.json(), indent=4, sort_keys=True))
+def main():
+    response = requests.get(endpoint_url(),params=get_params('artificial intelligence'),headers=get_headers())
+    if response.status_code !=200:
+        print(response.status_code)
+        print(response.text)
+    else:
+        # print(json.dumps(response.json(), indent=4, sort_keys=True))
+        with open('recent_tweets.csv','w', newline='', encoding="utf-8") as csvfile:
+            csvw = csv.writer(csvfile)
+            csvw.writerow(['tweet_id','author_id','text','entities'])
+            for item in response.json()['data']:
+                csvw.writerow([item['id'],item['author_id'],item['text'],json.dumps(item.get('entities','N/A'))])
 
+if __name__ == "__main__":
+    main()
