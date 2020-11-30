233fef638539ab20ac56f59a600eb17bbe56d627
amingheibi <amin.gheibi@gmail.com>
2020-11-29 23:44:50 -0500

    Dockerize

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import setting
 import requests
+from time import sleep
 
 from pathlib import Path
 from slack import get_slack_headers, post_to_slack
@@ -8,15 +9,23 @@ from utils import get_keywords, check_response, write_to_csv_file
 
 
 def main():
-    keywords = get_keywords()
-    for keyword in keywords:
-        print('Fetching tweets for ' + keyword)
-        response = requests.get(endpoint_url(), params=get_params(
-            keyword), headers=get_twitter_headers(setting.BEARER_TOKEN))
-        if check_response(response) == 0:
-            write_to_csv_file(response, Path('data/recent_tweets.csv'))
-            post_to_slack(response, setting.WEBHOOK)
+    while True:
+        keywords = get_keywords()
+        for keyword in keywords:
+            print('Fetching tweets for ' + keyword)
+            response = requests.get(endpoint_url(), params=get_params(
+                keyword), headers=get_twitter_headers(setting.BEARER_TOKEN))
+            if check_response(response) == 0:
+                write_to_csv_file(response, Path('data/recent_tweets.csv'))
+                post_to_slack(response, setting.WEBHOOK)
+        print("Sleeping for 10 hours")
+        sleep(36000)  # 10 hours
 
 
 if __name__ == "__main__":
     main()
+
+# TODO: Dockerize
+# TODO: Write unit tests
+# TODO: Add a cache or in-memory database to avoid fetching old tweets
+# TODO: Take different sleep time for different keywords
